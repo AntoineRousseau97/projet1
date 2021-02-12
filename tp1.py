21bd0500dcb81bf675ee7398ceb7aa16ae5593fb
Galap70 <78773513+Galap70@users.noreply.github.com>
2021-02-12 18:53:53 -0500

    Create tp1.py

---
+++ tp1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 18:10:19 2021
+
+@author: Gabriel
+"""
+
+import numpy as np
+import scipy as sp
+
+rho = 
+
+def n_e(rho):
+    return (rho * N * Z * Int_fif) / (2 * np.pi * r_e**2 * m_e * c**2 * 1/beta**2 * (np.ln((2 * m_e**2 * beta**2 * gamma**2 * T_max) / I**2) - 2 * beta**2))
